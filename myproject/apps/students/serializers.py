9d058eb37b7dc2a4021ad48a260414f6d5c23cb9
reshadMajumder <reshad22205341197@diu.edu.bd>
2025-10-13 00:09:10 +0600

    removed my project

--- myproject/apps/students/serializers.py
+++
@@ -1,8 +0,0 @@
-
-from rango.serializers import ModelSerializer
-from .models import Student
-
-class StudentSerializer(ModelSerializer):
-    class Meta:
-        model = Student
-        fields = ["id", "first_name", "last_name", "email", "age", "created_at"]
--- myproject/main.py
+++
@@ -1,4 +0,0 @@
-from project.asgi import app
-if __name__=='__main__':
- import uvicorn
- uvicorn.run("project.asgi:app", host='127.0.0.1', port=8000, reload=True)
