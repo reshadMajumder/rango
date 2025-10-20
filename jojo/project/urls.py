ebcee5522e383ff03071d400f7c08180e343ec81
reshadMajumder <reshad22205341197@diu.edu.bd>
2025-10-20 23:07:39 +0600

    removed the unused things

--- jojo/project/urls.py
+++
@@ -1,8 +0,0 @@
-from rango_api.router import Router
-from .views import HomeView
-from apps.joo.urls import router as joo_router
-
-router = Router()
-router.add("/", HomeView, methods=["GET"])
-
-router.include(joo_router)
--- jojo/project/views.py
+++
@@ -1,6 +0,0 @@
-from rango_api.generics import ListCreateView
-from starlette.responses import JSONResponse
-
-class HomeView:
-    async def get(self, request):
-        return JSONResponse({"message": "Welcome to Rango Framework!"})
